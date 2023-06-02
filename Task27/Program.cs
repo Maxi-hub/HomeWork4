e5dc7544838015405e82a50de109ed515e6e4d5d
Emma <em.lebedeva85@gmail.com>
2023-06-02 13:28:50 +0300

    Create Task27 & Task29

---
+++ Task27/Program.cs
@@ -0,0 +1,20 @@
+﻿// Программа, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+Console.WriteLine("Введите число");
+int number = Console.ReadLine();
+
+int Sum(string number);
+{
+    int sum = 0;
+    for(int i = 1; i < number.Length; i++)
+    {
+        sum = sum + number[i];
+    } 
+    Console.WriteLine($"Сумма цифр числа  + {number} = {sum}");
+    return sum;
+}
+
+int Sum(string number);
---
+++ Task29/Program.cs
@@ -0,0 +1,20 @@
+﻿// Программa, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+int[] array = new int[8];   
+
+
+void RandomNumber(int[] array)  
+{
+    Console.WriteLine("Введите числа");
+    for(int i = 0; i < array.Length; i++)
+    {
+        array[i] = Convert.ToInt32(Console.ReadLine());
+    }
+}
+
+RandomNumber(array);  
+Console.WriteLine("[" + String.Join (", ", array) + "]");
+
+
